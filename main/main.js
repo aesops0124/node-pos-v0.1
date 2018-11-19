68bcada79ea2be9a668994183d94e691298d1ba4
Tommy Hui <aesops0124@gmail.com>
2018-11-20 00:06:46 +0800

    Completed

--- main/main.js
+++ main/main.js
@@ -1,57 +1,64 @@
 module.exports = printReceipt;
 
 
-function checkExist(barcode, array) {
+function checkBarcodeExist(barcodeinstance, array) {
 	var found = false;
 	for(var i = 0; i < array.length; i++) {
-	    if (array[i].Name == barcode) {
+        var barcode = array[i]['Barcode'];
+	    if (barcode == (barcodeinstance)) {
 	        found = true;
 	        break;
 	    }
 	}
+	return found;
 };
 
 function printReceipt (inputs) {
 
 	var message = "";
-    message += '***<没钱赚商店>购物清单***\n';
+    message += '***<store earning no money>Receipt ***\n';
     var analyzed = [];
     var total = 0;
 
-    for (var product in inputs) {
-        if(!checkExist(product['barcode'], analyzed)) {
+    for (var i in inputs) {
+        if(!checkBarcodeExist(inputs[i]['Barcode'], analyzed)) {
             analyzed.push({
-                barcode: product['barcode'],
-                name: product['name'],
-                unit: product['unit'],
-                price: product['price'],
-                number: 1,
-                total: product['price']
+                Barcode: inputs[i]['Barcode'],
+                Name: inputs[i]['Name'],
+                Unit: inputs[i]['Unit'],
+                Price: inputs[i]['Price'],
+                Number: 1,
+                Subtotal: inputs[i]['Price']
             });
             
         }
         else {
-        	for (var i in analyzed) {
-			     if (analyzed[i]['barcode'] == barcode) {
-			        analyzed[i]['number'] = number+1;
-			        analyzed[i]['total'] = total+analyzed[i]['price'];
-			        break; //Stop this loop, we found it!
+        	for (var j in analyzed) {
+			     if (analyzed[j]['Barcode'] == inputs[i]['Barcode']) {
+			        analyzed[j]['Number'] += 1;
+			        analyzed[j]['Subtotal'] += analyzed[j]['Price'];
+			        break;
 			     }
 		   	}
-        
         }
     }
-    for (var product in analyzed) {
-        message += '名称：' + product['name'] + '，数量：' + product['number'] + product['unit'] + "，单价：" + product['price'] + "(元)，小计：" + product['total'] +'(元)\n';
-        total += product['total'];
+    for (var k in analyzed) {
+        message += 'Name: ' + analyzed[k]['Name'] + ', Quantity: ' + analyzed[k]['Number'] + " ";
+        if(analyzed[k]['Number'] < 2)
+            message += analyzed[k]['Unit'];
+        else
+            message += analyzed[k]['Unit'] + "s";
+         message += ", Unit price: " + parseFloat(analyzed[k]['Price']).toFixed(2) + " (yuan), Subtotal: " + parseFloat(analyzed[k]['Subtotal']).toFixed(2) + " (yuan)\n";
+ 
+        total += analyzed[k]['Subtotal'];
     }
 //
 
     message += '----------------------\n';
-    message += '总计：' + total + '(元)\n';
+    message += 'Total: ' + parseFloat(total).toFixed(2) + ' (yuan)\n';
 
 
-    message += '**********************';
+    message += '**********************\n';
     return message;
 };
 
